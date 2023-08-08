b150fef1234d9b591c8b949239a1a822abe3f901
tomas arias <tomas.arias2001@gmail.com>
2023-08-08 15:14:43 -0300

    add a new file with the ranking logic - js

--- js/script.js
+++ js/script.js
@@ -44,12 +44,7 @@ var modalContact = document.getElementById('modalContact');
 //abrir contacto-ranking
 var btnContact = document.getElementById('contact');
 var btnRanking = document.getElementById('ranking');
-//ranking
-var tableBody = document.getElementById('tableBody');
-var modalRanking = document.getElementById('modalRanking');
-var theadDate = document.getElementById('theadDate');
-var theadScore = document.getElementById('theadScore');
-var closeRanking = document.getElementById('closeRanking');
+
 
 /* INPUT ANTES DE ARRANCA */
 //capturo el input del nombre de usuario
@@ -325,69 +320,3 @@ closeRestart.addEventListener('click', closeRestartGame);
 function closeRestartGame() {
     modal.style.display = 'none';
 };
-
-/* LOCAL STORAGE */
-//funcion que trae el local storage y lo guada en la variable storage
-function getRanking() {
-    //traigo el rankign del local storage
-    if (localStorage.length !== 0) {
-        storage = JSON.parse(localStorage.getItem('games'));
-    };
-};
-
-//funcion que agrega un nueva nueva partida en el storage
-function pushStorage() {
-    getRanking();
-    var game = {
-        player: namePlayer,
-        score: score,
-        level: level,
-        datetime: new Date().toLocaleString()
-    };
-    storage.push(game);
-    //guardo los datos de la partida anterior
-    localStorage.setItem('games', JSON.stringify(storage));
-};
-
-//funcion que muestra el ranking
-btnRanking.addEventListener('click', function () {
-    modalRanking.style.display = 'block';
-    orderByScore();
-});
-
-//funcion que muestra la tabla
-function showRanking() {
-    tableBody.innerHTML = '';
-    for (var index = 0; index < storage.length; index++) {
-        var game = storage[index];
-        var row = tableBody.insertRow();
-        row.insertCell(0).innerText = game.player;
-        row.insertCell(1).innerText = game.score;
-        row.insertCell(2).innerText = game.level;
-        row.insertCell(3).innerText = game.datetime;
-    };
-};
-
-theadScore.addEventListener('click', orderByScore);
-//funcion que ordena la tabla por puntuacion
-function orderByScore() {
-    getRanking();
-    storage.sort(function (a, b) {
-        return b.score - a.score;
-    });
-    showRanking();
-};
-
-theadDate.addEventListener('click', orderByDate);
-//funcion que ordena la tabla por fecha
-function orderByDate() {
-    getRanking();
-    storage.sort(function (a, b) {
-        return Date.parse(b.datetime) - Date.parse(a.datetime);
-    });
-    showRanking();
-};
-
-closeRanking.addEventListener('click', function () {
-    modalRanking.style.display = 'none';
-});
